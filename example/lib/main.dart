389932c09908196cda2cb2ef8da9f79fbc375041
L Uttama <uttama@onnbikes.com>
2022-09-12 17:06:24 +0530

    Exaple update.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,45 +12,73 @@ class DemoApp extends StatefulWidget {
   DemoAppState createState() => DemoAppState();
 }
 
+double _value = 0.4;
+
 class DemoAppState extends State<DemoApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: const [
-          SizedBox(
-            height: 200,
-            width: 200,
-            child: CurvedCircularProgressIndicator(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const SizedBox(
+            height: 80,
           ),
-          SizedBox(
-            height: 200,
-            width: 200,
-            child: CurvedCircularProgressIndicator(
-              value: 0.5,
-              animationDuration: Duration(seconds: 3),
-              backgroundColor: Colors.amber,
-              color: Colors.red,
-              strokeWidth: 9,
+          const Center(
+            child: SizedBox(
+              height: 200,
+              width: 200,
+              child: CurvedCircularProgressIndicator(
+                strokeWidth: 12,
+              ),
             ),
           ),
-          SizedBox(
-            height: 200,
-            width: 200,
-            child: CurvedLinearProgressIndicator(),
+          Center(
+            child: SizedBox(
+              height: 200,
+              width: 200,
+              child: CurvedCircularProgressIndicator(
+                value: _value,
+                animationDuration: const Duration(seconds: 3),
+                backgroundColor: Colors.amber,
+                color: Colors.red,
+                strokeWidth: 9,
+              ),
+            ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 200,
             width: 200,
-            child: CurvedLinearProgressIndicator(
-              value: 0.5,
-              strokeWidth: 8,
-              backgroundColor: Colors.amber,
-              color: Colors.red,
+            child: Center(
+                child: CurvedLinearProgressIndicator(
+              strokeWidth: 12,
+            )),
+          ),
+          Center(
+            child: SizedBox(
+              height: 200,
+              width: 200,
+              child: CurvedLinearProgressIndicator(
+                value: _value,
+                strokeWidth: 8,
+                backgroundColor: Colors.amber,
+                color: Colors.red,
+              ),
             ),
           ),
         ],
       ),
+      floatingActionButton: IconButton(
+          onPressed: () {
+            setState(() {
+              if (_value >= 1.0) {
+                _value = 0;
+              } else {
+                _value += 0.05;
+              }
+            });
+          },
+          icon: const Icon(Icons.refresh)),
     );
   }
 }
